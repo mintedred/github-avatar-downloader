d10717fe6dc2200010b9b0dffe735a54a396bece
Corina Wong <hi@corinawong.com>
2017-10-09 18:12:51 +0000

    Create variables to fill out requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,13 @@
+var GITHUB_USER = "mintedred";
+var GITHUB_TOKEN = "ba4047015d75dd06ec6a2c1e8a3cabd0dfbb949b";
+
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
