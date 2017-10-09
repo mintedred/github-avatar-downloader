2d40877856fb24494f93649a44f41c48361134a4
Corina Wong <hi@corinawong.com>
2017-10-09 22:43:27 +0000

    Make the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,29 @@
 var GITHUB_USER = "mintedred";
-var GITHUB_TOKEN = "ba4047015d75dd06ec6a2c1e8a3cabd0dfbb949b";
-
+var GITHUB_T = process.env.GITHUB_TOKEN;
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ 'api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  var requestLink = {
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project',
+      "Authorization": "token " + GITHUB_T
+    },
+    url: requestURL
+  };
+
+  request.get(requestLink, function (error, response, body) {
+      console.log('statusCode:', response && response.statusCode);
+      if (error) {
+          console.log('error:', error);
+      } else {
+          console.log('body:', body);
+      }
+    });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
